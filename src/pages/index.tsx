64c575e80c241b4f4c64bb646efb78164ccbbc18
RobinRF <robinfiske87@gmail.com>
2023-01-21 13:49:50 +0100

    updated main header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,8 +12,8 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#50c878] to-[#006199]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-3xl font-extrabold tracking-tight text-white sm:text-[3.5rem]">
-            5-Card Generator
+          <h1 className="text-3xl font-extrabold tracking-tight text-white sm:text-[3rem]">
+            5-Card Poker Hand Generator
           </h1>
           <div className="m-5 flex grid-cols-1 items-center justify-center gap-4 bg-white p-5 sm:grid-cols-2 md:gap-8">
             <Cards />
